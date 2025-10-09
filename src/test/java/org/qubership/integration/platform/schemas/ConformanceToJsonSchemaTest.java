738894683484273f9b540d920b8b8e43c8d76c16
Pavels Kletnojs <pavels.kletnojs@netcracker.com>
2025-10-09 16:45:15 +0300

    feat: #57 normalize schema refs

--- src/test/java/org/qubership/integration/platform/schemas/ConformanceToJsonSchemaTest.java
+++ src/test/java/org/qubership/integration/platform/schemas/ConformanceToJsonSchemaTest.java
@@ -1,5 +1,7 @@
 package org.qubership.integration.platform.schemas;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.networknt.schema.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,14 +9,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import java.nio.charset.Charset;
-import java.util.Set;
-
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ConformanceToJsonSchemaTest {
     private static JsonSchema schema;
@@ -51,6 +53,23 @@ public class ConformanceToJsonSchemaTest {
         });
         assertAll(resource.getURI().getPath(), assertions.stream().map(
                 validationMessage -> () -> fail(validationMessage.getMessage())));
+
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("schemaResourceProvider")
+    public void testMandatoryFields(Resource resource) throws IOException {
+        String source = resource.getContentAsString(Charset.defaultCharset());
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        Map<String, Object> schemaYaml = mapper.readValue(source, Map.class);
+
+        List.of("$id", "$schema", "title").forEach(field ->
+                assertNotNull(schemaYaml.get(field), resource.getFilename() + ": '" + field + "' field must present!")
+        );
+
+        assertTrue(resource.getFilename().endsWith(".schema.yaml"), resource.getFilename() + ": Schema file name must end with .schema.yaml");
+        assertTrue(schemaYaml.get("$id").toString().endsWith(resource.getFilename()), resource.getFilename() + ": '$id' field must end with file name");
     }
 
     public static Stream<Resource> schemaResourceProvider() throws IOException {
