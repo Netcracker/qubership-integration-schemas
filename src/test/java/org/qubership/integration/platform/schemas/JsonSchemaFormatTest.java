75401e82bc5dfcc6a63840d21d07d91edcf4cf5a
Pavels Kletnojs <pavels.kletnojs@netcracker.com>
2025-10-20 15:02:43 +0300

    feat: extract handlers to separate schemas

--- src/test/java/org/qubership/integration/platform/schemas/JsonSchemaFormatTest.java
+++ src/test/java/org/qubership/integration/platform/schemas/JsonSchemaFormatTest.java
@@ -2,27 +2,35 @@ package org.qubership.integration.platform.schemas;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.FieldSource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class JsonSchemaFormatTest {
+    private static List<Resource> schemaResources;
+
+    @BeforeAll
+    public static void setUp() throws IOException {
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        schemaResources = Arrays.asList(resolver.getResources("classpath:**/*.schema.yaml"));
+    }
+
     @ParameterizedTest
-    @MethodSource("schemaResourceProvider")
-    public void testMandatoryFields(Resource resource) throws IOException {
+    @FieldSource("schemaResources")
+    public void testSchemaMandatoryFields(Resource resource) throws IOException {
         String source = resource.getContentAsString(Charset.defaultCharset());
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        Map<String, Object> schemaYaml = mapper.readValue(source, Map.class);
+        LinkedHashMap<String, Object> schemaYaml = mapper.readValue(source, LinkedHashMap.class);
 
         List.of("$id", "$schema", "title").forEach(field ->
                 assertNotNull(schemaYaml.get(field), resource.getFilename() + ": '" + field + "' field must present!")
@@ -32,9 +40,53 @@ public class JsonSchemaFormatTest {
         assertTrue(schemaYaml.get("$id").toString().endsWith(resource.getFilename()), resource.getFilename() + ": '$id' field must end with file name");
     }
 
-    public static Stream<Resource> schemaResourceProvider() throws IOException {
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        Resource[] resources = resolver.getResources("classpath:**/*.schema.yaml");
-        return Stream.of(resources);
+    @ParameterizedTest
+    @FieldSource("schemaResources")
+    public void testSchemaFieldsOrder(Resource resource) throws IOException {
+        String source = resource.getContentAsString(Charset.defaultCharset());
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        LinkedHashMap<String, Object> schemaYaml = mapper.readValue(source, LinkedHashMap.class);
+
+        List<String> listOfOrderedKeys = schemaYaml.sequencedKeySet().stream().filter(schemaFieldsOrder()::contains).toList();
+        List<String> rightOrderOfKeys = schemaFieldsOrder().stream().filter(listOfOrderedKeys::contains).toList();
+        assertEquals(rightOrderOfKeys, listOfOrderedKeys, resource.getFilename() + ": Schema fields order is incorrect!");
+    }
+
+    public static List<String> schemaFieldsOrder() {
+        return List.of(
+                "$id",
+                "type",
+                "allOf",
+                "title",
+                "description",
+                "metaInfo",
+                "properties"
+        );
+    }
+
+    @ParameterizedTest
+    @FieldSource("schemaResources")
+    public void testSchemaPropsMandatoryFields(Resource resource) throws IOException {
+        String source = resource.getContentAsString(Charset.defaultCharset());
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        LinkedHashMap<String, Object> schemaYaml = mapper.readValue(source, LinkedHashMap.class);
+
+        var props = (LinkedHashMap<String, LinkedHashMap<String, Object>>) schemaYaml.get("properties");
+
+        if (props != null) {
+            props.forEach((prop, propKeys) -> {
+                List<String> listOfOrderedPropKeys = propKeys.sequencedKeySet().stream().filter(propertiesFieldsOrder()::contains).toList();
+                List<String> rightOrderOfPropKeys = propertiesFieldsOrder().stream().filter(listOfOrderedPropKeys::contains).toList();
+                assertEquals(rightOrderOfPropKeys, listOfOrderedPropKeys, resource.getFilename() + ": Schema property '" + prop + "' fields order is incorrect!");
+            });
+        }
+    }
+
+    public static List<String> propertiesFieldsOrder() {
+        return List.of(
+                "type",
+                "properties",
+                "required"
+        );
     }
 }
